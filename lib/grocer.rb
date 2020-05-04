461560bd5191b78be6969a8ac8bc5745c446a36a
Kevin Neyer <kevinneyer@gmail.com>
2020-05-04 19:21:33 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,7 @@ while counter < cart.length do
    new_item = {
      :item => cart[counter][:item],
      :price => cart[counter][:price],
-     :clearance => cart[counter[:clearance],
+     :clearance => cart[counter][:clearance],
      :count => 1
    }
    new_cart << new_item
