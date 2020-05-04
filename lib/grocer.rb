5f914578ec3ffca6154238eef36003c2010ae60e
Kevin Neyer <kevinneyer@gmail.com>
2020-05-04 19:25:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
 counter = 0 
-while counter < collection.length do 
+while counter < collection.length
   if collection[counter][:name] == name
     return collection 
   end 
