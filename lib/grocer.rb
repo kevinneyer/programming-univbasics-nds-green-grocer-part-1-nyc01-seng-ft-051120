82a75630962915dfddd6aa0361d34ebdaca530b2
Kevin Neyer <kevinneyer@gmail.com>
2020-05-04 19:17:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,12 +9,14 @@ collection.find do |item_hash|
 end 
 
 def consolidate_cart(cart)
-new_array = []
+new_cart = []
 counter  = 0 
 while counter < cart.length do
- new_item = find_item_by_name_in_collection(cart[counter][:item], new_array)
+ new_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
  if new_item != nil 
-   return new_item
+   return new_item[:count] += 1 
+ else 
+   
  
  counter += 1
  end 
