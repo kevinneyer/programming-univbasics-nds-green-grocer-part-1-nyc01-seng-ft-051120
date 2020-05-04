8e4205780e4227cddd11ce7fed7411d4dff11ac1
Kevin Neyer <kevinneyer@gmail.com>
2020-05-04 18:47:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,6 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
 collection.find do |item_hash|
   if name == item_hash[:item] 
     return item_hash
