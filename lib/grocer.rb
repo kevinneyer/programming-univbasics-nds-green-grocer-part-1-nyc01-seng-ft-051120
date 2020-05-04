8f5fd33c7f01710ee53a095c0a6fcb7d1e009fb6
Kevin Neyer <kevinneyer@gmail.com>
2020-05-04 19:08:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,9 +11,8 @@ end
 def consolidate_cart(cart)
 new_array = []
 counter  = 0 
-
 while counter < cart.length do
- cart[counter]
+ new_item = find_item_by_name_in_collection
  
  counter += 1
  end 
